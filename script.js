8d1e1788e5e95981de3f26ac121ca687caf39c88
sakib-xrz <sakibxrz21@gmail.com>
2022-09-15 09:36:41 +0600

    project done

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ const start = () => {
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
-
+      countdownOverlay.innerHTML = "";
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
